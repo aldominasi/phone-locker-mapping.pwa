c9a19e6846bf329f8c0d1bb0cac38eb358756b2d
aldominasi <aldo.minasi@gmail.com>
2022-03-18 11:41:43 +0100

    aggiunto il margin left agli items della navbar

--- src/components/navbar/navBar.vue
+++ src/components/navbar/navBar.vue
@@ -8,16 +8,16 @@
       <b-navbar-toggle target="nav-collapse" class="mr-2"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item>
+          <b-nav-item class="ml-1">
             <router-link v-if="permessiUtente.readArmadi" :to="linkArmadi">Armadi</router-link>
           </b-nav-item>
-          <b-nav-item>
+          <b-nav-item class="ml-1">
             <router-link v-if="permessiUtente.readUtenti" :to="linkUtenti">Utenti</router-link>
           </b-nav-item>
-          <b-nav-item>
+          <b-nav-item class="ml-1">
             <router-link v-if="permessiUtente.writeUtenti" :to="linkRegistraUtente">Registra utente</router-link>
           </b-nav-item>
-          <b-nav-item>
+          <b-nav-item class="ml-1">
             <router-link :to="linkLogout">Esci</router-link>
           </b-nav-item>
         </b-navbar-nav>
