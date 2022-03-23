634f2f8702766c480671f1605f5327da965b22e7
aldominasi <aldo.minasi@gmail.com>
2022-03-23 13:19:42 +0100

    Update favicon

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,11 @@ module.exports = defineConfig({
   publicPath: '/plm/',
   pwa: {
     iconPaths: {
-      msTileImage: 'img/icons/mstile-150x150.png'
+      msTileImage: 'img/icons/mstile-150x150.png',
+      // favicon16: 'img/icons/favicon-16x16.png',
+      // favicon32: 'img/icons/favicon-32x32.png',
+      // appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      // maskIcon: 'img/icons/safari-pinned-tab.svg',
     },
     name: "Phone Locker Mapping",
     themeColor: "#218c60",
