74215db9fd639733122d156cf549996d757b3f5e
aldominasi <aldo.minasi@gmail.com>
2022-04-04 21:04:55 +0200

    bug fix modalità ricerca avanzata

--- src/components/home/PlmModificaArmadio.vue
+++ src/components/home/PlmModificaArmadio.vue
@@ -11,14 +11,17 @@
               <b-row>
                 <b-col cols="12">
                   <b-form-checkbox
-                    v-model="ricercaArmadi"
+                    class="ml-2 float-right"
+                    v-model="ricercaAvanzata"
                     name="check-button"
-                    switch>Ricerca l'armadio con l'ID</b-form-checkbox>
+                    size="lg"
+                    switch></b-form-checkbox>
+                  <label class="float-right mt-1">Cambia modalità</label>
                 </b-col>
               </b-row>
               <b-row class="mt-2">
                 <b-col cols="12">
-                  <plm-ricerca-armadi v-if="ricercaArmadi"></plm-ricerca-armadi>
+                  <plm-ricerca-armadi v-if="!ricercaAvanzata"></plm-ricerca-armadi>
                   <plm-ricerca-armadio v-else></plm-ricerca-armadio>
                 </b-col>
               </b-row>
@@ -39,6 +42,7 @@ import {
   BCardHeader,
   BCardBody,
   BFormCheckbox,
+  //BForm,
 } from 'bootstrap-vue';
 import PlmRicercaArmadi from '@/components/home/ricercaArmadi/PlmRicercaArmadi';
 import PlmRicercaArmadio from '@/components/home/ricercaArmadi/PlmRicercaArmadio';
@@ -54,11 +58,12 @@ export default {
     BCardHeader,
     BCardBody,
     BFormCheckbox,
+    //BForm,
   },
   name: "PlmModificaArmadio",
   data() {
     return {
-      ricercaArmadi: false,
+      ricercaAvanzata: false,
     }
   }
 }
--- src/components/home/ricercaArmadi/PlmRicercaArmadio.vue
+++ src/components/home/ricercaArmadi/PlmRicercaArmadio.vue
@@ -1,94 +1,83 @@
 <template>
   <div>
-    <b-container class="mt-10">
-      <b-row>
-        <b-col cols="12">
-          <b-form inline novalidate>
-            <label class="sr-only" for="inlineIdInput">ID armadio</label>
-            <b-form-input
-              id="inlineIdInput"
-              placeholder="Inserisci ID dell'armadio"
-              v-model="idArmadio"
-              class="inputCustomSecondary mb-2 mr-sm-2 mb-sm-0"></b-form-input>
-            <b-button
-              @click="getArmadio"
-              :disabled="!abilitaRicerca"
-              class="btnCustomPrimary">Cerca</b-button>
-          </b-form>
-        </b-col>
-        <b-col cols="12" class="mt-2" v-if="armadio != null">
+    <b-row>
+      <b-col cols="12">
+        <b-form novalidate class="text-center">
           <b-row>
-            <b-col cols="12">
-              <strong>Centrale: </strong>
-              <span>{{ armadio.centrale }}</span>
+            <b-col sm="12" md="10" lg="10" xl="10">
+              <b-form-input
+                placeholder="ID dell'armadio"
+                v-model="idArmadio"
+                class="inputCustomSecondary mb-2 mr-sm-2 mb-sm-0"></b-form-input>
             </b-col>
-            <b-col cols="12" class="mt-2">
-              <strong>Progressivo: </strong>
-              <span>{{ armadio.progressivo }}</span>
-            </b-col>
-            <b-col cols="12" class="mt-2">
-              <strong>Zona: </strong>
-              <span>{{ armadio.zona['info1'] }}</span>
-            </b-col>
-            <b-col cols="12" class="mt-2">
-              <strong>Tipo: </strong>
-              <span>{{ armadio.tipoArmadio }}</span>
-            </b-col>
-            <b-col cols="12" class="mt-2">
-              <strong>Indirizzo: </strong>
-              <span>{{ armadio.indirizzo }}</span>
-            </b-col>
-            <b-col cols="12" class="mt-2">
-              <strong>Note: </strong>
-              <span>{{ armadio.nota }}</span>
-            </b-col>
-          </b-row>
-          <b-row class="mt-2">
-            <b-col cols="12">
-              <l-map
-                ref="myMapInfo"
-                style="height: 300px"
-                :zoom="zoom"
-                :center="center">
-                <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-                <l-control position="bottomleft">
-                  <div class="legend">
-                    <h4>Legenda</h4>
-                    <i style="background-color: #2b82cb"></i>
-                    <span>Armadio</span>
-                  </div>
-                </l-control>
-                <l-marker v-if="armadio != null" :lat-lng="armadio.localizzazione.coordinates"></l-marker>
-              </l-map>
-            </b-col>
-          </b-row>
-          <b-row class="mt-2">
-            <b-col cols="12" class="text-center">
+            <b-col sm="12" md="2" lg="2" xl="2">
               <b-button
-                class="btnCustomPrimary"
-                @click="selezionaArmadio"></b-button>
+                @click="getArmadio"
+                :disabled="!abilitaRicerca"
+                class="btnCustomPrimary">Cerca</b-button>
             </b-col>
           </b-row>
-        </b-col>
-        <b-col cols="12" class="text-center mt-2" v-else>
-          <b-img src="/plm/img/empty.svg" width="100%" center fluid alt="Nessun dato"></b-img>
-          <label>Nessun Armadio</label>
-        </b-col>
-      </b-row>
-    </b-container>
+        </b-form>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <strong>Centrale: </strong>
+        <span>{{ armadio.centrale }}</span>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <strong>Progressivo: </strong>
+        <span>{{ armadio.progressivo }}</span>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <strong>Zona: </strong>
+        <span>{{ armadio.zona['info1'] }}</span>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <strong>Tipo: </strong>
+        <span>{{ armadio.tipoArmadio }}</span>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <strong>Indirizzo: </strong>
+        <span>{{ armadio.indirizzo }}</span>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <strong>Note: </strong>
+        <span>{{ armadio.nota }}</span>
+      </b-col>
+      <b-col cols="12" class="mt-2" v-if="armadio != null">
+        <l-map
+          ref="myMapInfo"
+          style="height: 300px;width: 100%"
+          :zoom="zoom"
+          :center="center">
+          <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+          <l-control position="bottomleft">
+            <div class="legend">
+              <h4>Legenda</h4>
+              <i style="background-color: #2b82cb"></i>
+              <span>Armadio</span>
+            </div>
+          </l-control>
+          <l-marker v-if="armadio != null" :lat-lng="armadio.localizzazione.coordinates"></l-marker>
+        </l-map>
+      </b-col>
+      <b-col cols="12" class="text-center mt-2" v-if="armadio != null">
+        <b-button
+          class="btnCustomPrimary"
+          @click="selezionaArmadio">Seleziona</b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 import {
-  BContainer,
   BRow,
   BCol,
   BForm,
   BFormInput,
-  BImg,
   BButton,
 } from 'bootstrap-vue';
+import 'leaflet/dist/leaflet.css';
 import {
   LMap,
   LTileLayer,
@@ -106,12 +95,10 @@ Icon.Default.mergeOptions({
 
 export default {
   components: {
-    BContainer,
     BRow,
     BCol,
     BForm,
     BFormInput,
-    BImg,
     BButton,
     LMap,
     LTileLayer,
@@ -140,6 +127,7 @@ export default {
           this.apiErrorHandler(response);
         else
           this.armadio = response.data.data;
+        this.center = response.data.data.localizzazione.coordinates;
       })
       .catch(() => { this.notificaErrore(); })
     },
